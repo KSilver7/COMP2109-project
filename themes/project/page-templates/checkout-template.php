1abcce456e6b848f492c2234ba20faff7de1f65f
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-09 15:51:32 -0400

    finished checkout template

--- themes/project/page-templates/checkout-template.php
+++ themes/project/page-templates/checkout-template.php
@@ -1,6 +1,16 @@
 <?php
 /**
- * Template Name:
- * Template Post Type:
+ * Template Name: Checkout Page
+ * Description: A custom template for the checkout page.
  */
 get_header();
+?>
+<section>
+    <?php
+        echo do_shortcode( '[woocommerce_checkout]');
+    ?>
+</section>
+
+<?php
+get_footer();
+?>

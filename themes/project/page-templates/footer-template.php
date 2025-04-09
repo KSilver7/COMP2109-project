06d509ffaba4b2f31997695356946b493fb9d73a
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-09 16:02:47 -0400

    finished footer-template

--- themes/project/page-templates/footer-template.php
+++ themes/project/page-templates/footer-template.php
@@ -1,6 +1,17 @@
 <?php
 /**
  * Template Name: Project Footer
- * Template Post Type: post
+ * Description: A custom footer template
  */
 get_header();
+$featuredImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+?>
+    <section class="project-footer-template">
+        <div style="background-image: url('<?php echo $featuredImg[0]; ?>');"></div>
+        <div>
+            <h4><?php the_title(); ?></h4>
+            <p><?php echo get_the_content(); ?></p>
+        </div>
+<?php
+get_footer();
+?>

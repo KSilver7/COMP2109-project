7488812f8d669c6528de7df0ebdf4d60b89464af
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-09 16:05:31 -0400

    finished header template

--- themes/project/page-templates/header-template.php
+++ themes/project/page-templates/header-template.php
@@ -1,6 +1,18 @@
 <?php
 /**
- * Template Name:
- * Template Post Type:
+ * Template Name: Header
+ * Decription: Project header template
  */
 get_header();
+$featuredImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+?>
+    <section class="project-header-template">
+        <div style="background-image: url('<?php echo $featuredImg[0]; ?>');"></div>
+        <div>
+<!-- title goes here-->
+            <h1><?php the_title(); ?></h1>
+            <p><?php echo get_the_content(); ?></p>
+        </div>
+<?php
+get_footer();
+?>

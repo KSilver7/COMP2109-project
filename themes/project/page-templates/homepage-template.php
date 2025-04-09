4c692db7a8a80517e71663fde400e36f110b2618
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-09 16:22:25 -0400

    Added in ACF, finished homepage template

--- themes/project/page-templates/homepage-template.php
+++ themes/project/page-templates/homepage-template.php
@@ -1,6 +1,26 @@
 <?php
 /**
- * Template Name:
- * Template Post Type:
+ * Template Name: Project Homepage
+ * Template Post Type: page
+ * Description: A custom homepage template.
  */
 get_header();
+?>
+<main>
+   <!-- This is the homepage template for the giant project-->
+   <section class="home-masthead" style="background-image: url('<?php echo wp_kses_post(get_field('masthead_image')); ?>')">
+        <div>
+            <h1><?php echo wp_kses_post(get_field('masthead_title'));?></h1>
+        </div>
+    </section>
+    <section class="home-row">
+        <div class="home-row-one">
+            <h2><?php echo wp_kses_post(get_field('row_one_title'));?></h2>
+            <p><?php echo wp_kses_post(get_field('row_one_text'));?></p>
+        </div>
+    </section>
+</main>
+<?php
+
+get_footer();
+?>

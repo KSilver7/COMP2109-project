e7ad8fd37a082f34a8671d13e33714afcfc45ec0
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-09 16:29:19 -0400

    finished post template

--- themes/project/page-templates/post-template.php
+++ themes/project/page-templates/post-template.php
@@ -1,6 +1,17 @@
 <?php
 /**
- * Template Name:
- * Template Post Type:
+ * Template Name: Project Post
+ * Template Post Type: post
  */
 get_header();
+$featuredImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+?>
+    <section class="project-post-template">
+        <div style="background-image: url('<?php echo $featuredImg[0]; ?>');"></div>
+        <div>
+            <h1><?php the_title(); ?></h1>
+            <p><?php echo get_the_content(); ?></p>
+        </div>
+<?php
+get_footer();
+?>
